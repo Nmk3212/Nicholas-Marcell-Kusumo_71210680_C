855bfb28473bd126f991b021fdd46d602b00ab0a
Nmk3212 <93307835+Nmk3212@users.noreply.github.com>
2022-09-19 20:09:36 +0700

    Update UG4_71210680.py

--- UG4_71210680.py
+++ UG4_71210680.py
@@ -1,15 +1,23 @@
 import json
 with open('mahasiswa.json', 'r') as file:
     mahasiswa = json.load(file)
+    
+    jumlah = int(input("Masukan jumlah mahasiswa baru : "))
+    
+    for i in range(0,jumlah):
+        nama = input("Masukan nama anda : ")
+        listnama = []
+        kosong = {}
+        jum_hobi = int(input("Masukan jumlah hobi : "))
+        listhobi = []
+        for j in range(1,jum_hobi+1):
+            hobi1 = input("Masukan Hobi ke-"+ str(j) +": ")
+            listhobi.append(hobi1)
+        prestasi = input("Masukan prestasi anda : ")
+        listnama.append({"BioData":{"Hobi":listhobi,"Prestasi":prestasi}})
+        mahasiswa[nama] = listnama
+        print("=== Data Berhasil ditambahkan ===")
+        print()
 
-jumlah = int(input("Masukan jumlah mahasiswa baru : "))
-for i in range(jumlah):
-    nama = input("Masukan nama anda : ")
-    jum_hobi = int(input("Masukan jumlah hobi : "))
-    for j in range(1,jum_hobi+1):
-        hobi1 = input("Masukan Hobi "+ str(j) +": ")
-    prestasi = input("Masukan prestasi anda : ")
-    print("=== Data Berhasil ditambahkan ===")
-
-with open('mahasiswa.json', 'a') as file:
+with open('mahasiswa.json', 'w') as file:
     json.dump(mahasiswa, file)
